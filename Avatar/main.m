361b0aec726b286352687ca5474e7518ccd460e2
Alex Edunov <alexander.edunov@gmail.com>
2015-11-21 11:56:45 +0300

    Initial commit

---
+++ Avatar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Avatar
+//
+//  Created by Alex Edunov on 21/11/15.
+//  Copyright © 2015 AlexEdunov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
